27b91d2d74c96d049579044aacdcd359e2f6a47e
Mitran Marius <grognak16@gmail.com>
2026-07-01 20:24:37 +0300

    can now create terrain and it adjusts automatically yippie

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -4,8 +4,10 @@ extends CharacterBody2D
 @onready var tsm = $Animation/AnimationTree.get("parameters/ToolStateMachine/playback")
 
 var direction: Vector2
-var speed := 50
-var current_tool: Enum.Tool = Enum.Tool.AXE
+var speed := 200
+var current_tool: Enum.Tool
+var current_seed: Enum.Seed
+var can_move: bool = true
 
 func move():
 	direction = Input.get_vector("left", "right", "up", "down")
@@ -15,9 +17,11 @@ func move():
 func get_basic_input():
 	if Input.is_action_just_pressed("tool_backward") or Input.is_action_just_pressed("tool_forward"):
 		var dir = Input.get_axis("tool_backward", "tool_forward")
-		current_tool = posmod((current_tool + int(dir)), Enum.Tool.size())
-		
-		print(current_tool)
+		current_tool = posmod((current_tool + int(dir)), Enum.Tool.size()) as Enum.Tool
+	
+	if Input.is_action_just_pressed("seed_forward"):
+		current_seed = posmod(current_seed + 1, Enum.Seed.size()) as Enum.Seed
+		print(current_seed)
 	if Input.is_action_just_pressed("action"):
 		tsm.travel(Data.TOOL_STATE_ANIMATIONS[current_tool])
 		$Animation/AnimationTree.set("parameters/ToolOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
@@ -38,6 +42,14 @@ func tool_use_emit():
 	print('tool')
 
 func _physics_process(_delta: float) -> void:
-	get_basic_input()
-	move()
-	animate()
+	if can_move:
+		get_basic_input()
+		move()
+		animate()
+
+
+func _on_animation_tree_animation_started(_anim_name: StringName) -> void:
+	can_move = false
+
+func _on_animation_tree_animation_finished(_anim_name: StringName) -> void:
+	can_move = true
